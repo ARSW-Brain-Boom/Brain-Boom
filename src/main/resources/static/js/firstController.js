9af704897442d435392b57d08b261b1152ea1884
Estevan-21 <bravar06@hotmail.com>
2018-11-28 09:17:02 -0500

    Avance Nickname

--- src/main/resources/static/js/firstController.js
+++ src/main/resources/static/js/firstController.js
@@ -4,6 +4,7 @@ function saveNick(){
   nickName=document.getElementById('nick').value;
   console.log(nickName);
   savePlayers(nickName);
-  location.href='selectMap.html'; 
+  location.href='selectMap.html';
+  //a 
 }
 
