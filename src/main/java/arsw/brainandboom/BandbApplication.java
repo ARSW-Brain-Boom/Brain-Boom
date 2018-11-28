68237c7a7eb4d7b78b8b90f992f9ac0685b16c92
Jonathan Prieto <jonnhi.prietou@gmail.com>
2018-11-28 00:04:17 -0500

    Creacion de bloques solidos y blandos de manera aleatoria. Mejoras en la creacion y animacion de las bombas. Colisiones parcialmente implementadas

--- src/main/java/arsw/brainandboom/BandbApplication.java
+++ src/main/java/arsw/brainandboom/BandbApplication.java
@@ -3,10 +3,14 @@ package arsw.brainandboom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author Brain&Boom Team
+ */
 @SpringBootApplication
 public class BandbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BandbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BandbApplication.class, args);
+    }
 }
