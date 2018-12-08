a0e5c8c8286b7c494bc0a27c55024b9b2534f446
Estevan-21 <bravar06@hotmail.com>
2018-12-08 14:48:10 -0500

    Lista jugadores al API

--- src/main/resources/static/js/firstController.js
+++ src/main/resources/static/js/firstController.js
@@ -4,6 +4,13 @@ var room=0;
 function saveNick(){
   nickName=document.getElementById('nick').value;
   room+=1;  
+  axios.post('/bnb/player', nickName)
+	  .then(function (response) {
+	    console.log(response);
+	  })
+	  .catch(function (error) {
+	    console.log(error);
+	});
   savePlayers(nickName);
   console.log(nickName);
   localStorage.setItem("room",room); 
